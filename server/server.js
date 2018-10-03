8da401f35acdb00cd5b4840ce1cab76fe089050f
Lawrence <Lawrence4code@gmail.com>
2018-10-03 18:33:47 +0530

    Completed basic server for routing and authentication

--- server/server.js
+++ server/server.js
@@ -19,10 +19,83 @@ app.use(cookieParser());
 // Models
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 // middlewares
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
+
+// ***************************************** Products ****************************************** //
+
+// By arrival
+// article?sortBy=createdAt&order=desc&liimit=4
+
+app.get('/api/product/articles', (req, res) => {
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+  Product.find().populate('brand').populate('wood').sort([[sortBy, order]]).limit(limit).exec((err, articles) => {
+    if (err) return res.status(400).send(err);
+    res.send(articles);
+  })
+
+})
+
+// By sell
+
+// article?sortBy=sold&order=desc&liimit=4
+
+app.get('/api/product/articles_by_id', (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+  if (type === 'array') {
+    let ids = req.query.id.split(',');
+    items = [];
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item);
+    });
+
+    Product.find({ "_id": { $in: items } }).populate('brand').populate('wood').exec((err, docs) => {
+      if (err) return res.json({ success: false, err })
+      return res.status(200).send(docs);
+    });
+  }
+});
+
+app.post('/api/product/article', auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc
+    });
+  });
+});
+
+// ******************************************* Wood ******************************************** //
+
+app.post('/api/product/wood', auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+
+    res.status(200).json({
+      success: true,
+      wood: doc
+    });
+  });
+});
+
+app.get('/api/product/woods', (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(woods);
+  });
+});
 
 // ******************************************* Brand ******************************************* //
 
@@ -96,7 +169,7 @@ app.post('/api/user/login', (req, res) => {
 });
 
 app.get('/api/user/logout', auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, doc) => {
+  User.findOneAndUpdate({ "_id": req.user._id }, { token: '' }, (err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).send({ success: true });
   });
@@ -107,3 +180,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at port ${port}.`);
 });
+
+// ./mongod --dbpath ~/mongo/data
+// ./mongo
